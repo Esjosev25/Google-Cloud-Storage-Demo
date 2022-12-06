db67cd75ebc83a82373ba8f8ec194c93cb74a7e2
Estuardo Villeda <evilleda@bdgsa.net>
2022-12-06 13:25:33 -0600

    remove apikey

--- routes/api/storage.js
+++ routes/api/storage.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const { Storage } = require('@google-cloud/storage');
 const { v4: uuidv4 } = require('uuid');
@@ -5,10 +6,8 @@ const router = express.Router();
 
 
 
-const storage = new Storage({
-  keyFilename: 'APIKey.json'
-});
-
+const storage = new Storage();
+//upload file
 router.post('/file', async (req, res) => {
   const { name } = req.query;
   if (!name)
@@ -19,10 +18,9 @@ router.post('/file', async (req, res) => {
     const generationMatchPrecondition = 0;
     const options = {
       destination: name,
-      preconditionOpts: { ifGenerationMatch: generationMatchPrecondition },
     };
 
-    await storage.bucket(bucketName).upload(`inputs/${name}`, options);
+    await storage.bucket(bucketName).upload(`./inputs/${name}`, options);
     res.send(`${name} uploaded to ${bucketName}`)
   } catch (error) {
     console.error(error);
@@ -30,8 +28,6 @@ router.post('/file', async (req, res) => {
 
 });
 const bucketName = 'vision-bucket-arca'
-
-
 
 router.delete('/file', async (req, res) => {
   const { name } = req.query;
@@ -49,14 +45,14 @@ router.delete('/file', async (req, res) => {
     res.status(500).send(error)
   }
 })
-
+//download file
 router.get('/file', async (req, res) => {
   const { name } = req.query;
   if (!name)
     return res.status(400).send('please send a name')
   try {
     const options = {
-      destination:  name,
+      destination:  './outputs/'+name,
     };
     // Downloads the file
     await storage.bucket(bucketName).file(name).download(options);
@@ -67,9 +63,62 @@ router.get('/file', async (req, res) => {
   }
 
 });
+//move file
+router.put('/file', async (req, res) => {
+  const { name } = req.query;
+  if (!name)
+    return res.status(400).send('please send a name')
+  try {
+    const moveOptions = {
+      preconditionOpts: {
+        ifGenerationMatch: 0,
+      },
+    };
+    console.log('hola')
+    await storage
+      .bucket(bucketName)
+      .file(name)
+      .move('salida/1/pdf.pdf', moveOptions);
+    res.send(`gs://${bucketName}/${name} downloaded to ${'./outputs/cloudStorage' + name}.`)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error)
+  }
 
+});
+//rename
+router.patch('/file', async (req, res) => {
+  const { srcFileName, destFileName } = req.query;
+  if (!srcFileName)
+    return res.status(400).send('please send a name')
+  try {
+    await storage.bucket(bucketName).file(srcFileName).rename(destFileName);
+    res.send(`gs://${bucketName}/${srcFileName} renamed to gs://${bucketName}/${destFileName}.`)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error)
+  }
 
+});
 
+//get metadata
+router.get('/metadata', async (req, res) => {
+  // const { srcFileName, destFileName } = req.query;
+  // if (!srcFileName)
+  //   return res.status(400).send('please send a name')
+  // try {
+  //   const [metadata] = await storage.bucket(bucketName).getMetadata();  
+  //   res.send(`gs://${bucketName}/${srcFileName} renamed to gs://${bucketName}/${destFileName}.`)
+  // } catch (error) {
+  //   console.error(error)
+  //   res.status(500).send(error)
+  // }
 
+});
 
+//post file on stream
+router.post('/stream', async (req, res) => {
+  
+
+});
 module.exports = router;
